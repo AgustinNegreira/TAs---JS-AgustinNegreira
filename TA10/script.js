a627837a5ace21d5024eb0a5f34018ea2576f041
AgustinNegreira <anegreira2004@gmail.com>
2025-04-02 18:42:39 -0300

    TA10 finished

--- TA10/script.js
+++ TA10/script.js
@@ -1,8 +1,8 @@
-function getOdds(nums) {
-    const odds = nums.filter(num => num % 2 !== 0);
-    return odds;
+function getSum(nums) {
+    let sum = nums.reduce((acc, num) => acc + num, 0);
+    return sum;
 }
 
-const numbers = [1,56,2,4,7,9,345,341,1416,73572,3,5,6,32523]
+const numbers = [1,2,3,4,5]
 
-console.log(getOdds(numbers));
+console.log(getSum(numbers)); 
--- TA9/script.js
+++ TA9/script.js
@@ -1,8 +1,8 @@
-function getSum(nums) {
-    let sum = nums.reduce((acc, num) => acc + num, 0);
-    return sum;
+function getOdds(nums) {
+    const odds = nums.filter(num => num % 2 !== 0);
+    return odds;
 }
 
 const numbers = [1,56,2,4,7,9,345,341,1416,73572,3,5,6,32523]
 
-console.log(getSum(numbers));
+console.log(getOdds(numbers));
